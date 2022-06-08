596c1fb7f5df707c3e1b49dbecbea771abc5275c
Leynier Gutiérrez González <leynier41@gmail.com>
2022-06-08 17:55:36 -0400

    fix: test enums for clean metadata before and after

--- tests/test_enums.py
+++ tests/test_enums.py
@@ -4,6 +4,7 @@ import uuid
 from sqlalchemy import create_mock_engine
 from sqlalchemy.sql.type_api import TypeEngine
 from sqlmodel import Field, SQLModel
+from sqlmodel.main import default_registry
 
 """
 Tests related to Enums
@@ -12,30 +13,6 @@ Associated issues:
 * https://github.com/tiangolo/sqlmodel/issues/96
 * https://github.com/tiangolo/sqlmodel/issues/164
 """
-
-
-class MyEnum1(enum.Enum):
-    A = "A"
-    B = "B"
-
-
-class MyEnum2(enum.Enum):
-    C = "C"
-    D = "D"
-
-
-class BaseModel(SQLModel):
-    id: uuid.UUID = Field(primary_key=True)
-    enum_field: MyEnum2
-
-
-class FlatModel(SQLModel, table=True):
-    id: uuid.UUID = Field(primary_key=True)
-    enum_field: MyEnum1
-
-
-class InheritModel(BaseModel, table=True):
-    pass
 
 
 def pg_dump(sql: TypeEngine, *args, **kwargs):
@@ -57,16 +34,66 @@ sqlite_engine = create_mock_engine("sqlite://", sqlite_dump)
 
 
 def test_postgres_ddl_sql(capsys):
+    SQLModel.metadata.clear()
+    default_registry.dispose()
+
+    class MyEnum1(enum.Enum):
+        A = "A"
+        B = "B"
+
+    class MyEnum2(enum.Enum):
+        C = "C"
+        D = "D"
+
+    class BaseModel(SQLModel):
+        id: uuid.UUID = Field(primary_key=True)
+        enum_field: MyEnum2
+
+    class FlatModel(SQLModel, table=True):
+        id: uuid.UUID = Field(primary_key=True)
+        enum_field: MyEnum1
+
+    class InheritModel(BaseModel, table=True):
+        pass
+
     SQLModel.metadata.create_all(bind=postgres_engine, checkfirst=False)
 
     captured = capsys.readouterr()
     assert "CREATE TYPE myenum1 AS ENUM ('A', 'B');" in captured.out
     assert "CREATE TYPE myenum2 AS ENUM ('C', 'D');" in captured.out
 
+    SQLModel.metadata.clear()
+    default_registry.dispose()
+
 
 def test_sqlite_ddl_sql(capsys):
+    SQLModel.metadata.clear()
+    default_registry.dispose()
+
+    class MyEnum1(enum.Enum):
+        A = "A"
+        B = "B"
+
+    class MyEnum2(enum.Enum):
+        C = "C"
+        D = "D"
+
+    class BaseModel(SQLModel):
+        id: uuid.UUID = Field(primary_key=True)
+        enum_field: MyEnum2
+
+    class FlatModel(SQLModel, table=True):
+        id: uuid.UUID = Field(primary_key=True)
+        enum_field: MyEnum1
+
+    class InheritModel(BaseModel, table=True):
+        pass
+
     SQLModel.metadata.create_all(bind=sqlite_engine, checkfirst=False)
 
     captured = capsys.readouterr()
     assert "enum_field VARCHAR(1) NOT NULL" in captured.out
     assert "CREATE TYPE" not in captured.out
+
+    SQLModel.metadata.clear()
+    default_registry.dispose()
